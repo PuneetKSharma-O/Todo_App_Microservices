98ebeadd8ac8f7e662a075d4346dfafefa497e37
puneetsdo <puneetsharmab@outlook.com>
2025-11-13 09:02:34 +0530

    chore: Uncomment backend configuration in provider.tf and SQL database variable definitions in variable.tf for clarity

--- Environment/dev/variable.tf
+++ Environment/dev/variable.tf
@@ -53,29 +53,29 @@ variable "acr" {
   
 }
 
-variable "sql_db_server" {
-    description = "A map of SQL Database Server configurations."
-    type = map(object({
-        name                         = string
-        resource_group_name          = string
-        location                     = string
-        version                      = string
-        administrator_login          = string
-        administrator_login_password = string
-        minimum_tls_version          = optional(string)
-    }))
+# variable "sql_db_server" {
+#     description = "A map of SQL Database Server configurations."
+#     type = map(object({
+#         name                         = string
+#         resource_group_name          = string
+#         location                     = string
+#         version                      = string
+#         administrator_login          = string
+#         administrator_login_password = string
+#         minimum_tls_version          = optional(string)
+#     }))
   
-}
-variable "sql_db" {
-  description = "A map of SQL Database configurations."
-  type = map(object({
-    name         = string
-    collation    = optional(string)
-    license_type = optional(string)
-    max_size_gb  = optional(number)
-    sku_name     = optional(string)
-    enclave_type = optional(string)
-    tags         = optional(map(string))
-  }))
+# }
+# variable "sql_db" {
+#   description = "A map of SQL Database configurations."
+#   type = map(object({
+#     name         = string
+#     collation    = optional(string)
+#     license_type = optional(string)
+#     max_size_gb  = optional(number)
+#     sku_name     = optional(string)
+#     enclave_type = optional(string)
+#     tags         = optional(map(string))
+#   }))
 
-}
+# }
