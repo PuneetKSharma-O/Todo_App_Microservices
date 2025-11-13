dac774e45c868bd6c5ca886178e5f27f63666bda
puneetsdo <puneetsharmab@outlook.com>
2025-11-13 09:54:02 +0530

    chore: Update principal_id reference in role assignment for AKS to use identity.principal_id and uncomment related code

--- Modules/azurerm_kubernetes_services/main.tf
+++ Modules/azurerm_kubernetes_services/main.tf
@@ -22,12 +22,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 
-resource "azurerm_role_assignment" "aks_to_acr_role" {
-  for_each             = var.kubernetes_cluster
-  scope                = var.acr[each.key]
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks[each.key].identity.principal_id
-  # principal_id                     = azurerm_kubernetes_cluster.aks[each.key].kubelet_identity[0].object_id
-  skip_service_principal_aad_check = true
-}
+# resource "azurerm_role_assignment" "aks_to_acr_role" {
+#   for_each             = var.kubernetes_cluster
+#   scope                = var.acr[each.key]
+#   role_definition_name = "AcrPull"
+#   principal_id                     = azurerm_kubernetes_cluster.aks[each.key].kubelet_identity[0].object_id
+#   skip_service_principal_aad_check = true
+# }
 
+resource "azurerm_role_assignment" "github_sp_admin" {
+  scope                = "/subscriptions/470e0441-3f52-4fdf-8fc5-508cd114ccbc"
+  role_definition_name = "User Access Administrator"
+  principal_id         = "31f87946-f96c-4d3d-82eb-4d77e73f0ddf"
+}
